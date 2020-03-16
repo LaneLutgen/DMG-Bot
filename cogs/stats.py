c7604eca2a7925f7480208327f51aee9b6b09879
unknown <20230450+HDR@users.noreply.github.com>
2020-03-16 08:08:23 +0100

    Update stats.py
    Convert to embed, and handle multiple cores and threads.

--- cogs/stats.py
+++ cogs/stats.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+
+import discord
 from discord.ext import commands
 import psutil
 import os
@@ -14,11 +17,12 @@ class Stats(commands.Cog):
 	@commands.command()
 	async def stats(self, ctx):
 		"""Reports server stats."""
-		
+		embed = discord.Embed(title="Server Statistics", color=discord.Color.green())
+		embed.add_field(name="Uptime", value=str(timedelta(seconds=int(time.time() - start_time))), inline=False)
+		embed.add_field(name="Cores", value=psutil.cpu_count(logical=False), inline=True)
+		embed.add_field(name="Threads", value=psutil.cpu_count(), inline=True)
+		embed.add_field(name="Usage", value=str(psutil.cpu_percent(interval=None, percpu=True)).replace(",", "%").replace("[", "").replace("]", "%"), inline=False)
 		await ctx.channel.trigger_typing()
-		
-		temps = psutil.sensors_temperatures()
-		await ctx.send('**Runtime:** ' + str(int(round(time.time() - start_time, 10) / (60))) + ' Minutes \n**CPU Load:** ' + str(round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()),2)) + '% \n' + '**CPU Temps:** \n' + str("Core 0: " + "+" + str(temps)[46:50] + "C\n" + "Core 1: " + "+" + str(temps)[112:116] + "C\n" + "Core 2: " + "+" + str(temps)[178:182] + "C\n" + "Core 3: " + "+" + str(temps)[244:248] + "C") + '\n' + '**RAM Usage:** ' + str(psutil.virtual_memory().percent) + '%')
-		
+		await ctx.send(embed=embed)
 def setup(bot):
 	bot.add_cog(Stats(bot))
