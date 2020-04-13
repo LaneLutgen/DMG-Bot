6a2f08fefeff3a6638ab77e4233fe6200d063603
unknown <20230450+HDR@users.noreply.github.com>
2020-04-13 15:09:40 +0200

    Price Checker & API Updates
    Added the market api cog, this exports pinned messages from the marked channel for displaying on external websites
    
    Updated the price checker to allow navigating trough multiple results using reactions

--- cogs/api.py
+++ cogs/api.py
@@ -1,4 +1,6 @@
 import json
+
+import discord
 from github import Github
 import traceback
 from discord.ext import commands
@@ -28,17 +30,36 @@ class api(commands.Cog):
         channel = guild.get_channel(int(get_section("api").get("pinnedChannel")))
         await self.bot.wait_until_ready()
         cron = CronTab(interval)
+        repo = Github(get_section("api").get("gitKey")).get_repo(get_section("api").get("gitRepo"))
+        obj = []
+        file = repo.get_contents("market.json")
+        repo.update_file("market.json", str(datetime.datetime.now()), json.dumps(obj), file.sha)
         while True:
-            repo = Github(get_section("api").get("gitKey")).get_repo(get_section("api").get("gitRepo"))
-            obj = []
             async for msg in channel.history(limit=5000):
                 if(msg.pinned):
-                    tmp = {"user": str(msg.author), "message": str(msg.content).replace("\n", " ")}
+                    if msg.embeds:
+                        eurl = []
+                        eurl.clear()
+                        for embed in msg.embeds:
+                            eurl.append(str(embed.url))
+
+                    if msg.attachments:
+                        aurl = []
+                        for attachment in msg.attachments:
+                            aurl.append(str(attachment.url))
+
+                    else:
+                        eurl = ""
+                    tmp = {"user": str(msg.author), "message": str(msg.content), "created": str(msg.created_at), "avatar_url": str(msg.author.avatar_url), "message_id": str(msg.id), "embeds": str(eurl), "attachments": str(aurl)}
                     obj.append(tmp)
+                    eurl = ""
+                    aurl = ""
             file = repo.get_contents("market.json")
             repo.update_file("market.json", str(datetime.datetime.now()), json.dumps(obj), file.sha)
             print("Updated market.json")
             await asyncio.sleep(cron.next(default_utc=True))
+            obj.clear()
+            tmp.clear()
 
 
 def setup(bot):
--- cogs/pricechecker.py
+++ cogs/pricechecker.py
@@ -23,9 +23,13 @@ class Pricechecker(commands.Cog):
                 if getresult["products"][0]["loose-price"]/100 != 0.0: embed.add_field(name="Loose Price:", value="$"+str(getresult["products"][0]["loose-price"]/100), inline=True)
                 if getresult["products"][0]["cib-price"]/100 != 0.0: embed.add_field(name="CIB Price:", value="$"+str(getresult["products"][0]["cib-price"]/100), inline=True)
                 if getresult["products"][0]["new-price"]/100 != 0.0: embed.add_field(name="NEW Price:", value="$"+str(getresult["products"][0]["new-price"]/100), inline=True)
+                embed.add_field(name="Search Query", value=message, inline=True)
+                embed.add_field(name="Result", value="1/" + str(len(getresult["products"])), inline=True)
                 embed.add_field(name="Get more info about this game", value="https://www.pricecharting.com/game/" + str(getresult["products"][0]["id"]), inline=False)
-                await ctx.channel.trigger_typing()
-                await ctx.send(embed=embed)
+                await ctx.trigger_typing()
+                msg = await ctx.send(embed=embed)
+                await msg.add_reaction(emoji = "\u2B05")
+                await msg.add_reaction(emoji = "\u27A1")
         else: await ctx.send("Something went wrong, please try again")
 
     async def getData(self, game):
@@ -39,12 +43,46 @@ class Pricechecker(commands.Cog):
                     return js
                 else: return None
 
+    async def changeCheckPage(self, search: str, channel: discord.TextChannel, message: discord.Message, author_id: int = "", page: int = 0):
+        getresult = await self.getData(search)
+        if getresult["status"] == "success":
+            if not getresult["products"]: await channel.send("No results found, please try redefining your search.")
+            else:
+                if page <= int(len(getresult["products"])-1):
+                    embed = discord.Embed(title=getresult["products"][page]["product-name"], color=discord.Color.teal())
+                    embed.add_field(name="Console", value=getresult["products"][page]["console-name"], inline=False)
+                    if getresult["products"][page]["loose-price"]/100 != 0.0: embed.add_field(name="Loose Price:", value="$"+str(getresult["products"][page]["loose-price"]/100), inline=True)
+                    if getresult["products"][page]["cib-price"]/100 != 0.0: embed.add_field(name="CIB Price:", value="$"+str(getresult["products"][page]["cib-price"]/100), inline=True)
+                    if getresult["products"][page]["new-price"]/100 != 0.0: embed.add_field(name="NEW Price:", value="$"+str(getresult["products"][page]["new-price"]/100), inline=True)
+                    embed.add_field(name="Search Query", value=search, inline=True)
+                    embed.add_field(name="Result", value=str(page + 1) + "/" + str(len(getresult["products"])), inline=True)
+                    embed.add_field(name="Get more info about this game", value="https://www.pricecharting.com/game/" + str(getresult["products"][page]["id"]), inline=False)
+                    await message.edit(embed=embed)
+        else: await channel.send("Something went wrong, please try again")
+
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please specify a game you want to search for")
         elif isinstance(error, commands.CommandOnCooldown):
             await ctx.send('Sorry, the command is on cooldown for you right now. Try again in ' + str(int(error.retry_after)) + ' seconds')
+
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        channel = self.bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        embed = message.embeds[0].fields
+        page = str(embed[len(embed)-2].value).split('/')
+        if payload.emoji.name == "\u2B05": #Left
+            if payload.member.id != self.bot.user.id:
+                await message.remove_reaction(payload.emoji, payload.member)
+                if int(page[0]) > 1:
+                    await self.changeCheckPage(embed[len(embed)-3].value, channel, message,  page=int(page[0])-2)
+        if payload.emoji.name == "\u27A1": #Right
+            if payload.member.id != self.bot.user.id:
+                await message.remove_reaction(payload.emoji, payload.member)
+                await self.changeCheckPage(embed[len(embed)-3].value, channel, message,  page=int(page[0]))
 
 
 def setup(bot):
